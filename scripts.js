cfaae854058135e905c63a5a03d5b3d762b6a558
cindykandie <cindyjk6@gmail.com>
2022-12-19 21:42:27 +0300

    add javascript to the navbar

--- scripts.js
+++ scripts.js
@@ -1,17 +1,18 @@
-let img = document.querySelector(".hamburger_menu")
+let img = document.getElementById("hamburger-menu")
 let menu = document.querySelector(".menu-bar")
 
-img.addEventListener('click', toggleNav)
 
 function toggleNav() {
     if (img.src.match("images/Union.svg")) {
-      img.src = "images/icon.svg";
+      img.src = "images/Icon.svg";
       menu.classList.toggle("active")
+      console.log('red')
       
     } 
     else{
       img.src = "images/Union.svg";
       menu.classList.toggle("active")
-      
+      console.log('blue')
     }
   }
+  img.addEventListener('click', toggleNav)
