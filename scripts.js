1325ef94a06cda923cec169f468547a012323975
Joshua Lyngdoh <joshualyngdoh52@gmail.com>
2022-12-20 13:37:04 +0530

    Checked for linter errors

--- scripts.js
+++ scripts.js
@@ -1,25 +1,21 @@
-let img = document.getElementById("hamburger-menu")
-let menu = document.querySelector(".menu-bar")
-let specs = document.querySelectorAll(".specs")
-
+const img = document.getElementById('hamburger-menu');
+const menu = document.querySelector('.menu-bar');
+const specs = document.querySelectorAll('.specs');
 
 function toggleNav() {
-    if (img.src.match("images/Union.svg")) {
-      img.src = "images/Icon.svg";
-      menu.classList.toggle("active")
-    } 
-    else{
-      img.src = "images/Union.svg";
-      menu.classList.toggle("active")
-    }
+  if (img.src.match('images/Union.svg')) {
+    img.src = 'images/Icon.svg';
+    menu.classList.toggle('active');
+  } else {
+    img.src = 'images/Union.svg';
+    menu.classList.toggle('active');
   }
-  function closeMenu() {
-    menu.classList.toggle("active")
-    img.src = "images/Union.svg";
-  }
-  img.addEventListener('click', toggleNav)
-  // specs.addEventListener('click', closeMenu)
-  specs.forEach((element) => {
-    element.addEventListener('click', closeMenu)})
-
-
+}
+function closeMenu() {
+  menu.classList.toggle('active');
+  img.src = 'images/Union.svg';
+}
+img.addEventListener('click', toggleNav);
+specs.forEach((element) => {
+  element.addEventListener('click', closeMenu);
+});
