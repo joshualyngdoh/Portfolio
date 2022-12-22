2e5e63f29dc18873b73bfab584e89b69769a46ac
Joshua Lyngdoh <joshualyngdoh52@gmail.com>
2022-12-22 19:15:38 +0530

    Changes to HTML and JS files

--- scripts.js
+++ scripts.js
@@ -222,3 +222,33 @@ window.addEventListener('load', () => {
     });
   });
 });
+
+//validate form section
+
+const email = document.getElementById('email');  
+document.querySelector('#press').addEventListener('click', (e) => {
+  const name = document.getElementById('name');
+    if (!name.validity.valid) {
+      document.getElementById('logi').innerHTML = 'Form not sent! Kindly input valid name';
+      e.preventDefault();
+      return;
+    }
+    if (email.validity.typeMismatch) {
+      document.getElementById('logi').innerHTML = 'Form not sent! Kindly input valid email.';
+      e.preventDefault();
+      return;
+    }
+    if (email.validity.patternMismatch) {
+      document.getElementById('logi').innerHTML = 'Form not sent! Kindly input email in lowercase';
+      e.preventDefault();
+      return;
+    }
+    const text = document.getElementById('messagebox');
+    if (!text.validity.valid) {
+      document.getElementById('logi').innerHTML = 'Form not sent! Kindly input valid message';
+      e.preventDefault();
+      return;
+    }
+    document.getElementById('cont').press();
+  });
+
