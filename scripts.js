3714f4e9624850ea69421916265dc185b845ed76
Joshua Lyngdoh <joshualyngdoh52@gmail.com>
2022-12-22 14:21:30 +0530

    Checked for linters error

--- scripts.js
+++ scripts.js
@@ -1,94 +1,94 @@
-const img = document.getElementById("hamburger-menu");
-const menu = document.querySelector(".menu-bar");
-const specs = document.querySelectorAll(".specs");
+const img = document.getElementById('hamburger-menu');
+const menu = document.querySelector('.menu-bar');
+const specs = document.querySelectorAll('.specs');
 const popUpModalWrapper = document.querySelector('#pop-up-window');
 
 function toggleNav() {
-  if (img.src.match("images/Union.svg")) {
-    img.src = "images/Icon.svg";
-    menu.classList.toggle("active");
+  if (img.src.match('images/Union.svg')) {
+    img.src = 'images/Icon.svg';
+    menu.classList.toggle('active');
   } else {
-    img.src = "images/Union.svg";
-    menu.classList.toggle("active");
+    img.src = 'images/Union.svg';
+    menu.classList.toggle('active');
   }
 }
 function closeMenu() {
-  menu.classList.toggle("active");
-  img.src = "images/Union.svg";
+  menu.classList.toggle('active');
+  img.src = 'images/Union.svg';
 }
-img.addEventListener("click", toggleNav);
+img.addEventListener('click', toggleNav);
 specs.forEach((element) => {
-  element.addEventListener("click", closeMenu);
+  element.addEventListener('click', closeMenu);
 });
 
 // Project object
 const projects = [
   {
-    id: "proj1",
-    name: "Tonic",
-    background: "CANOPY",
-    stack: "Back End Dev",
-    year: "2015",
-    image: "images/Snapshoot Portfolio.svg",
-    alt: "Snapshoot Portfolio image",
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    id: 'proj1',
+    name: 'Tonic',
+    background: 'CANOPY',
+    stack: 'Back End Dev',
+    year: '2015',
+    image: 'images/Snapshoot Portfolio.svg',
+    alt: 'Snapshoot Portfolio image',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     excerpt:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    tools: ["html", "css", "javascript"],
-    submitButton: "work-section-button",
-    liveLink: "#",
-    sourceLink: "#",
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    tools: ['html', 'css', 'javascript'],
+    submitButton: 'work-section-button',
+    liveLink: '#',
+    sourceLink: '#',
   },
   {
-    id: "proj2",
-    name: "Multi-Post Stories",
-    background: "CANOPY",
-    stack: "Back End Dev",
-    year: "2015",
-    image: "images/Snapshoot Portfolio (1).svg",
-    alt: "Snapshoot Portfolio image",
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    id: 'proj2',
+    name: 'Multi-Post Stories',
+    background: 'CANOPY',
+    stack: 'Back End Dev',
+    year: '2015',
+    image: 'images/Snapshoot Portfolio (1).svg',
+    alt: 'Snapshoot Portfolio image',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     excerpt:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    tools: ["html", "css", "javascript"],
-    liveLink: "#",
-    sourceLink: "#",
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    tools: ['html', 'css', 'javascript'],
+    liveLink: '#',
+    sourceLink: '#',
   },
   {
-    id: "proj3",
-    name: "Tonic",
-    background: "CANOPY",
-    stack: "Back End Dev",
-    year: "2015",
-    image: "images/Snapshoot Portfolio (2).svg",
-    alt: "Snapshoot Portfolio image",
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    id: 'proj3',
+    name: 'Tonic',
+    background: 'CANOPY',
+    stack: 'Back End Dev',
+    year: '2015',
+    image: 'images/Snapshoot Portfolio (2).svg',
+    alt: 'Snapshoot Portfolio image',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     excerpt:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    tools: ["html", "css", "javascript"],
-    liveLink: "#",
-    sourceLink: "#",
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    tools: ['html', 'css', 'javascript'],
+    liveLink: '#',
+    sourceLink: '#',
   },
   {
-    id: "proj4",
-    name: "Multi-Post Stories",
-    background: "CANOPY",
-    stack: "Back End Dev",
-    year: "2015",
-    image: "images/Snapshoot Portfolio (3).svg",
-    alt: "Snapshoot Portfolio image",
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    id: 'proj4',
+    name: 'Multi-Post Stories',
+    background: 'CANOPY',
+    stack: 'Back End Dev',
+    year: '2015',
+    image: 'images/Snapshoot Portfolio (3).svg',
+    alt: 'Snapshoot Portfolio image',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     excerpt:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    tools: ["html", "css", "javascript"],
-    liveLink: "#",
-    sourceLink: "#",
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    tools: ['html', 'css', 'javascript'],
+    liveLink: '#',
+    sourceLink: '#',
   },
 ];
 
 // Popup modal
 function popUpModal(project) {
-  let modalContent = `
+  const modalContent = `
   <div class="window-blur"></div>
   <div class="window-container">
     <div class="window-body">
@@ -133,13 +133,13 @@ function popUpModal(project) {
       </div>
     </div>
   </div>
-  `
+  `;
   return modalContent;
 }
 
 // Tools template
 function getToolsTemplate(tools) {
-  let toolsContent = "";
+  let toolsContent = '';
 
   tools.forEach((tool) => {
     toolsContent += `
@@ -148,13 +148,13 @@ function getToolsTemplate(tools) {
   });
 
   return toolsContent;
-};
+}
 
 // Project cards template
 function loadProjectCards(projects = []) {
-  let projectContents = "";
+  let projectContents = '';
 
-  projects.map((project) => {
+  projects.forEach((project) => {
     projectContents += `
     <div class="page-container">
     <div class="img-wrap">
@@ -191,37 +191,36 @@ function loadProjectCards(projects = []) {
   });
 
   return projectContents;
-};
-
-function closePopUp() {
-  popUpModalWrapper.style.display = 'none'
 }
 
-function openPopUp (project) {
+function closePopUp() {
+  popUpModalWrapper.style.display = 'none';
+}
+
+function openPopUp(project) {
   const popUpTemplate = popUpModal(project);
   popUpModalWrapper.innerHTML = popUpTemplate;
   popUpModalWrapper.style.display = 'block';
 
-  const popUpModalCloseButton = document.querySelector("#pop-up-close-button");
+  const popUpModalCloseButton = document.querySelector('#pop-up-close-button');
 
-  popUpModalCloseButton.addEventListener('click', closePopUp)
+  popUpModalCloseButton.addEventListener('click', closePopUp);
 }
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   const portfolioSection = document.querySelector('#portfolio');
   popUpModalWrapper.style.display = 'none';
 
   portfolioSection.innerHTML = loadProjectCards(projects);
 
   const projectButtons = document.querySelectorAll('#see-project-button');
-  
-  Array.from(projectButtons).map(element => {
+
+  Array.from(projectButtons).forEach((element) => {
     element.addEventListener('click', () => {
       const projectID = element.getAttribute('data-project-id');
-      const projectObj = projects.find(project => project.id === projectID)
+      const projectObj = projects.find((project) => project.id === projectID);
 
       openPopUp(projectObj);
-    })
-  }) 
+    });
+  });
 });
-
